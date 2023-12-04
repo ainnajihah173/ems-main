2c71473bf2ac19d09e2ad5e6d6b68e32cdebb82e
ainnajihah <ainnajihah173@gmail.com>
2023-12-04 11:18:32 +0800

    first commit

---
+++ app/Http/Controllers/WitnessController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WitnessController extends Controller
+{
+    public function index(){
+
+    }
+    public function manage(){
+        
+    }
+    public function create(){
+        
+    }
+    public function store(){
+        
+    }
+    public function update(){
+        
+    }
+    public function edit(){
+        
+    }
+    public function show(){
+        
+    }
+    public function destroy(){
+        
+    }
+}
